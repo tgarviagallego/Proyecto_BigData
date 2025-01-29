085bd1857610a30ed24899e4c8525f757c93e914
Isabel Morell <202207975@alu.comillas.edu>
2025-01-29 18:47:47 +0100

    codigo terminado y datos descargados

--- download_data.py
+++ download_data.py
@@ -18,10 +18,20 @@ if __name__ == "__main__":
         "SHIBUSD", 
         "DOTUSD", 
         "AAVEUSD", 
-        "XMLUSD"
+        "XLMUSD"
     ]
 
     for crypto in cryptos:
-        os.makedirs(f"{folder_path}/{crypto[:-3]}", exist_ok=True)
+        crypto_path = f"{folder_path}/{crypto[:-3]}"
+        os.makedirs(crypto_path, exist_ok=True)
         crypto_data = request.get_hist(symbol=crypto, exchange="CRYPTO", interval=Interval.daily, n_bars=4*365+1)
         
+        for year, year_data in crypto_data.groupby(crypto_data.index.year):
+            for month, month_data in year_data.groupby(year_data.index.month):
+                month_path = f"{crypto_path}/{year}/{month}"
+                os.makedirs(month_path, exist_ok=True)
+
+                file_name = f"{crypto[:-3]}_{year}_{month}.csv"
+                file_path = os.path.join(month_path, file_name)
+                
+                month_data.to_csv(file_path, index=True)
