a69c93da4958b6aea675e18406a565622beac29e
Isabel Morell <202207975@alu.comillas.edu>
2025-02-05 19:59:32 +0100

    creamos y guardamos los datos en una bbdd d glue

---
+++ src/create_bbdd.py
@@ -0,0 +1,63 @@
+import boto3
+from credenciales import *
+
+BUCKET_NAME = "trading-view-data"
+AWS_REGION = "eu-south-2"
+
+DATABASE_NAME = "trade_data_imat3a02"
+
+glue_client = boto3.client(
+    "glue",
+    aws_access_key_id=KEY_ID,
+    aws_secret_access_key=ACCESS_KEY,
+    aws_session_token=SESSION_TOKEN,
+    region_name=AWS_REGION
+)
+
+if __name__ == "__main__":
+    # Creamos la base de datos
+    try:
+        glue_client.create_database(
+            DatabaseInput = {
+                "Name": DATABASE_NAME,
+                "Description": "Base de datos con los datos de trading view"
+            }
+        )
+    except Exception as error:
+        print(error)
+
+    # Creamos el crawler
+    cryptos = [
+        "BTCUSD", 
+        "ETHUSD", 
+        "XRPUSD", 
+        "SOLUSD", 
+        "DOGEUSD", 
+        "ADAUSD", 
+        "SHIBUSD", 
+        "DOTUSD", 
+        "AAVEUSD", 
+        "XLMUSD"
+    ]
+
+    crawler_name_prefix = "crawler_trading_view_"
+    iam_role = "trading-view-IAM-role"
+
+    for crypto in cryptos:
+        s3_targets = []
+
+        for anio in range(2021, 2026):
+            s3_targets.append({"Path": f"s3://{BUCKET_NAME}/{crypto}/{anio}"})
+
+        crawler_name = f"{crawler_name_prefix}{crypto}"
+
+        glue_client.create_crawler(
+            Name=crawler_name,
+            Role=iam_role,
+            DatabaseName=DATABASE_NAME,
+            Targets={'S3Targets': s3_targets},
+            TablePrefix=f"trade_data_{crypto}_",
+            Description=f"Crawler que detecta datos de {crypto} y los guarda en la BBDD"
+        )
+
+        glue_client.start_crawler(Name=crawler_name)
